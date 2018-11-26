f821b38f55e650d5361ea5df37b05dd43d376704
little67 <little67@yandex.ru>
2018-11-26 23:18:04 +0300

    init commit

---
+++ book-service/src/main/java/com/tsc/learning/netflix/bookservice/config/MainConfig.java
@@ -0,0 +1,14 @@
+package com.tsc.learning.netflix.bookservice.config;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class MainConfig {
+    @Bean
+    public RestTemplate rest(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+}
