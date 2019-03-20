2d54daf4e9cb177968dbff94aa8412afce256fcc
Albert_Kamalov <akamalov@tsconsulting.ru>
2019-03-20 17:07:55 +0300

    Add gateway-service module, easy route to task-service

---
+++ HELP.md
@@ -0,0 +1,10 @@
+# Getting Started
+
+### Guides
+The following guides illustrate how to use some features concretely:
+
+* [Circuit Breaker](https://spring.io/guides/gs/circuit-breaker/)
+* [Routing and Filtering](https://spring.io/guides/gs/routing-and-filtering/)
+* [Client Side Load Balancing with Ribbon and Spring Cloud](https://spring.io/guides/gs/client-side-load-balancing/)
+* [Using Spring Cloud Gateway](https://github.com/spring-cloud-samples/spring-cloud-gateway-sample)
+
--- reading-service/src/main/java/com/tsc/learning/netflix/reading/ReadingServiceApplication.java
+++ reading-service/src/main/java/com/tsc/learning/netflix/reading/ReadingServiceApplication.java
@@ -1,17 +1,15 @@
 package com.tsc.learning.netflix.reading;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
-import com.tsc.learning.netflix.reading.config.RibbonConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -21,13 +19,9 @@ import java.net.URI;
 @EnableCircuitBreaker
 @SpringBootApplication
 @RestController
-@RibbonClient(
-        name = "task-service",
-        configuration = RibbonConfig.class)
 public class ReadingServiceApplication {
 
     @Bean
-    @LoadBalanced
     RestTemplate restTemplate() {
         return new RestTemplate();
     }
@@ -36,10 +30,13 @@ public class ReadingServiceApplication {
     @Qualifier("restTemplate")
     RestTemplate restTemplate;
 
-    @RequestMapping("/taskList")
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @GetMapping("/taskList")
     @HystrixCommand(fallbackMethod = "defaultTask")
     public String taskList() {
-        URI uri = URI.create("http://task-service/taskList");
+        URI uri = URI.create("http://localhost:8082/taskList");
         return restTemplate.getForObject(uri, String.class);
     }
 
